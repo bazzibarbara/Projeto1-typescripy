7c59ae6233ed7c91a0e0974c986e81401d64a6a1
unknown <barbararamosberno@outlook.com>
2023-05-12 08:03:37 -0300

    feat typescripty

--- src/domains/Artistas/controllers/ArtistaController.ts
+++ src/domains/Artistas/controllers/ArtistaController.ts
@@ -1,69 +1,70 @@
-const router = require('express').Router();
-const ArtistaService = require('../services/ArtistaService');
+import express, { Request, Response } from 'express';
+import { ArtistaService } from '../services/ArtistaService';
 
-router.get('/all', async(req,res) =>{
-    try {
-        const artistas = await ArtistaService.obterArtistas();
-        res.status(200).send(artistas);
-    } catch {
-        res.status(400);
-    }
+const router = express.Router();
+
+router.get('/all', async (req: Request, res: Response): Promise<Response> => {
+  try {
+    const artistas = await ArtistaService.obterArtistas();
+    return res.status(200).send(artistas);
+  } catch (error) {
+    return res.status(400);
+  }
 });
 
-router.get('/all/:nome', async (req, res) => {
-    const { nome } = req.params;
+router.get('/all/:nome', async (req: Request, res: Response): Promise<Response> => {
+  const { nome } = req.params;
 
-    try{
-        const artista = await ArtistaService.obterArtistaPorNome(nome);
-        res.status(200).send(artista);
-    }catch{
-        res.status(400).json('Artista nao encontrado.');
-    }
+  try {
+    const artista = await ArtistaService.obterArtistaPorNome(nome);
+    return res.status(200).send(artista);
+  } catch (error) {
+    return res.status(400).json('Artista nao encontrado.');
+  }
 });
 
-router.get('/all/:nome/musicas', async (req, res) => {
-    const { nome } = req.params;
+router.get('/all/:nome/musicas', async (req: Request, res: Response): Promise<Response> => {
+  const { nome } = req.params;
 
-    try{
-        const musica = await ArtistaService.obterMusicasPorArtista(nome);
-        res.status(200).send(musica);
-    }catch{
-        res.status(400).json('Artista nao encontrado.');
-    }
+  try {
+    const musica = await ArtistaService.obterMusicasPorArtista(nome);
+    return res.status(200).send(musica);
+  } catch (error) {
+    return res.status(400).json('Artista nao encontrado.');
+  }
 });
 
-router.post('/add', async(req,res) =>{
-    const body = req.body;
-    try {
-        await ArtistaService.adicionarArtista(body);
-        return res.status(201).json('Artista criado com sucesso');
-    } catch {
-        return res.status(400);
-    }
+router.post('/add', async (req: Request, res: Response): Promise<Response> => {
+  const body = req.body;
+
+  try {
+    await ArtistaService.adicionarArtista(body);
+    return res.status(201).json('Artista criado com sucesso');
+  } catch (error) {
+    return res.status(400);
+  }
 });
 
-router.put('/edit/:nome/:novafoto', async (req, res) => {
-    const { nome, novafoto } = req.params;
-    
-    try{
-        await ArtistaService.editarFoto(nome, novafoto);
-        res.status(200).send(`Foto do artista ${nome} editado com sucesso.`);
-    }catch{
-        res.status(400).json();
-    }
+router.put('/edit/:nome/:novafoto', async (req: Request, res: Response): Promise<Response> => {
+  const { nome, novafoto } = req.params;
 
+  try {
+    await ArtistaService.editarFoto(nome, novafoto);
+    return res.status(200).send(`Foto do artista ${nome} editado com sucesso.`);
+  } catch (error) {
+    return res.status(400).json();
+  }
 });
 
-router.delete('/delete/:nome', async (req, res) => {
-    const { nome } = req.params;
-    
-    try{
-        await ArtistaService.deletarArtista(nome);
-        res.status(200).send('Artista deletado com sucesso');
-    }
-    catch{
-        res.status(400).send('Artista nao encontrado.');
-    }
+router.delete('/delete/:nome', async (req: Request, res: Response): Promise<Response> => {
+  const { nome } = req.params;
+
+  try {
+    await ArtistaService.deletarArtista(nome);
+    return res.status(200).send('Artista deletado com sucesso');
+  } catch (error) {
+    return res.status(400).send('Artista nao encontrado.');
+  }
 });
 
-module.exports = router;
+export { router };
