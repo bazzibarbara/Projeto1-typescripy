07e0b06eb4bd980ee7100ecb4fbd338239598fc2
Bárbara Ramos <127155092+bazzibarbara@users.noreply.github.com>
2023-05-19 11:59:54 -0300

    Update error-handler.ts
    feat typescript

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -3,26 +3,27 @@ import InvalidParamError from '../../errors/InvalidParamError';
 import NotAuthorizedError from '../../errors/NotAuthorizedError';
 import TokenError from '../../errors/TokenError';
 import QueryError from '../../errors/QueryError';
-import { internalServerError, badRequest, forbidden, notFound } from '../../constants/statusCodes';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import statusCodes from '../../constants/statusCodes';
 
 function errorHandler(error: Error, req: Request, res: Response, next: NextFunction): void {
   let message = error.message;
-  let status = internalServerError;
-  
+  let status = statusCodes.internalServerError;
+
   if (error instanceof InvalidParamError) {
-    status = badRequest;
+    status = statusCodes.badRequest;
   }
 
-  if (error instanceof NotAuthorizedError) {
-    status = forbidden;
+  if (error instanceof NotAuthorizedError || error instanceof JsonWebTokenError) {
+    status = statusCodes.forbidden;
   }
 
   if (error instanceof TokenError) {
-    status = notFound;
+    status = statusCodes.notFound;
   }
 
   if (error instanceof QueryError) {
-    status = badRequest;
+    status = statusCodes.badRequest;
   }
 
   console.log(error);
