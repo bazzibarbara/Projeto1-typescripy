38dc42aca1eb94ef9aa50660b513b083d8935bd1
Bárbara Ramos <127155092+bazzibarbara@users.noreply.github.com>
2023-05-19 12:11:44 -0300

    Update Musica.ts

--- src/domains/Musicas/models/Musica.ts
+++ src/domains/Musicas/models/Musica.ts
@@ -1,45 +1,61 @@
-const database = require('../../../../database/index');
-const {DataTypes} = require('sequelize');
-const Artista = require('../../Artistas/models/Artista');
+import { DataTypes, Model } from 'sequelize';
+import database from '../../../../database/index';
+import Artista from '../../Artistas/models/Artista';
 
-const Musica = database.define('Musica', {
+class Musica extends Model {
+  public id!: number;
+  public foto!: string;
+  public titulo!: string;
+  public categoria!: string;
+  public idArtista!: number;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Musica.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-
     foto: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-
     titulo: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-
     categoria: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-});
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    idArtista: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: database,
+    modelName: 'Musica',
+  }
+);
 
 Artista.hasMany(Musica, {
-    foreignKey: 'idArtista'
+  foreignKey: 'idArtista',
 });
 
 Musica.belongsTo(Artista, {
-    constraint: true,
-    foreignKey: 'idArtista'
+  constraint: true,
+  foreignKey: 'idArtista',
 });
 
+Musica.sync({ alter: false, force: false })
+  .then(() => {
+    console.log('Tabela de Musicas foi (re)criada');
+  })
+  .catch((err) => console.log(err));
 
-Musica.sync({alter: false, force: false})
-    .then(() => {
-        console.log('Tabela de Musicas foi (re)criada');
-    })
-    .catch((err) => console.log(err));
-
-module.exports = Musica;
+export default Musica;
