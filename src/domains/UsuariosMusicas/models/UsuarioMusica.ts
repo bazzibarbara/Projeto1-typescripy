92dba1252b6e046dfc715453fd1ac4a676096f80
Bárbara Ramos <127155092+bazzibarbara@users.noreply.github.com>
2023-05-19 12:20:05 -0300

    Update UsuarioMusica.ts

--- src/domains/UsuariosMusicas/models/UsuarioMusica.ts
+++ src/domains/UsuariosMusicas/models/UsuarioMusica.ts
@@ -1,40 +1,38 @@
-const database = require('../../../../database/index');
-const {DataTypes} = require('sequelize');
-const Usuario = require('../../Usuarios/models/Usuario');
-const Musica = require('../../Musicas/models/Musica');
+import { DataTypes } from 'sequelize';
+import database from '../../../../database/index';
+import Usuario from '../../Usuarios/models/Usuario';
+import Musica from '../../Musicas/models/Musica';
 
 const UsuarioMusica = database.define('UsuarioMusica', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  idMusica: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Musica,
+      key: 'id',
     },
-
-    idMusica: {
-        type: DataTypes.INTEGER,
-        references:{
-            model: Musica,
-            key: 'id'
-        }
+  },
+  idUsuario: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Usuario,
+      key: 'id',
     },
-
-    idUsuario: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Usuario,
-            key: 'id'
-        }
-    }
+  },
 });
 
 Musica.belongsToMany(Usuario, { through: UsuarioMusica });
 Usuario.belongsToMany(Musica, { through: UsuarioMusica });
 
-UsuarioMusica.sync({alter: false, force: false})
-    .then(() => {
-        console.log('Tabela de UsuarioMusicas foi (re)criada');
-    })
-    .catch((err) => console.log(err));
+UsuarioMusica.sync({ alter: false, force: false })
+  .then(() => {
+    console.log('Tabela de UsuarioMusicas foi (re)criada');
+  })
+  .catch((err) => console.log(err));
 
-module.exports = UsuarioMusica;
+export default UsuarioMusica;
