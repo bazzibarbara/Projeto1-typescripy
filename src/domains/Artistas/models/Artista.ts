5f26ef0e5b23917f79c148bc17717bc984dfee32
Bárbara Ramos <127155092+bazzibarbara@users.noreply.github.com>
2023-05-19 12:08:16 -0300

    Update Artista.ts

--- src/domains/Artistas/models/Artista.ts
+++ src/domains/Artistas/models/Artista.ts
@@ -1,34 +1,52 @@
-const database = require('../../../../database/index');
-const {DataTypes} = require('sequelize');
+import database from '../../../../database/index';
+import { DataTypes, Model } from 'sequelize';
 
-const Artista = database.define('Artista', {
+interface ArtistaAttributes {
+  id: number;
+  nome: string;
+  nacionalidade: string;
+  foto: string;
+}
+
+class Artista extends Model<ArtistaAttributes> implements ArtistaAttributes {
+  public id!: number;
+  public nome!: string;
+  public nacionalidade!: string;
+  public foto!: string;
+}
+
+Artista.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
     },
-    
     nome: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
-
     nacionalidade: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
-
     foto: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     }
-});
+  },
+  {
+    sequelize: database,
+    modelName: 'Artista',
+    tableName: 'Artistas'
+  }
+);
 
-Artista.sync({alter: false, force: false})
-    .then(() => {
-        console.log('Tabela de Artistas foi (re)criada');
-    })
-    .catch((err) => console.log(err));
+Artista.sync({ alter: false, force: false })
+  .then(() => {
+    console.log('Tabela de Artistas foi (re)criada');
+  })
+  .catch((err: Error) => console.log(err));
 
-module.exports = Artista;
+export default Artista;
