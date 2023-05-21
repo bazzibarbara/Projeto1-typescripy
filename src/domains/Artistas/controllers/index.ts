b471d94e1e5979ecaa6929eaa84c1bae1deca9b6
unknown <barbararamosberno@outlook.com>
2023-05-21 11:10:47 -0300

    feat Artista controllers

--- src/domains/Artistas/controllers/index.ts
+++ src/domains/Artistas/controllers/index.ts
@@ -1,12 +1,12 @@
-const router = require('express').Router();
-const ArtistaService = require('../services/ArtistaService');
-const {verifyJWT, checkRole } = require('../../../middlewares/auth-middlewares.js');
-const statusCodes = require('../../../../constants/statusCodes.js');
-const userRoles = require('../../Usuarios/constants/userRoles');
-
+import { Router, Request, Response, NextFunction } from 'express';
+import {ArtistaService} from '../services/ArtistaService';
+import {verifyJWT, checkRole } from'../../../middlewares/auth-middlewares.js';
+import {userRoles} from '../../Usuarios/constants/userRoles';
+import { statusCodes } from '../../../../constants/statusCodes';
+export const router = Router();
 router.get('/all', 
     verifyJWT,
-    async(req,res, next) =>{
+    async(req:Request, res:Response, next:NextFunction) =>{
         try {
             const artistas = await ArtistaService.obterArtistas();
             res.status(statusCodes.success).send(artistas);
@@ -18,7 +18,7 @@ router.get('/all',
 
 router.get('/all/:nome', 
     verifyJWT,
-    async(req, res, next) =>{
+    async(req:Request, res:Response, next:NextFunction) =>{
         const { nome } = req.params;
         try{
             const artista = await ArtistaService.obterArtistaPorNome(nome);
@@ -31,7 +31,7 @@ router.get('/all/:nome',
 
 router.get('/all/:nome/musicas',
     verifyJWT,
-    async(req, res, next) =>{
+    async(req:Request, res:Response, next:NextFunction) =>{
         const { nome } = req.params;
 
         try{
@@ -45,7 +45,7 @@ router.get('/all/:nome/musicas',
 
 router.post('/add',
     verifyJWT,
-    async(req, res, next) => {
+    async(req:Request, res:Response, next:NextFunction) => {
         const body = req.body;
         try {
             await ArtistaService.adicionarArtista(body);
@@ -59,7 +59,7 @@ router.post('/add',
 router.put('/:id',
     verifyJWT,
     checkRole([userRoles.admin]),
-    async (req, res, next) => {
+    async (req:Request, res:Response, next:NextFunction) => {
         try{
             await ArtistaService.editarArtista(req.params.id, req.body);
             res.status(statusCodes.success).end();
@@ -72,7 +72,7 @@ router.put('/:id',
 router.delete('/delete/:id',
     verifyJWT,
     checkRole([userRoles.admin]),
-    async (req, res, next) => {
+    async (req:Request, res:Response, next:NextFunction) => {
         try{
             await ArtistaService.deletarArtista(req.params.id);
             res.status(statusCodes.success).end();
